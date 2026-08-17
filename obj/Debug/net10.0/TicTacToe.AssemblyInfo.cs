10e5a98b26320951905876f622609f0ad093286c
Suyal Sahukhal <suyalsahukhala787@gmail.com>
2026-08-17 19:02:25 +0545

    feat: implement core Tic-Tac-Toe game logic for .NET console app

--- obj/Debug/net10.0/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/net10.0/TicTacToe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839c2702ee6ce30dea46e8d0833ed958e57b595f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f9ab6814b79dedac53706c392d6a607b8d980a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
